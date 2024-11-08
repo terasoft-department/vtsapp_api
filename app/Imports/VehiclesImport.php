50686439834de43954b93336e3f32e5a51b61e95
mvc <nyemamudhihir@gmail.com>
2024-11-08 20:40:26 +0300

    add all imports logc excell

--- app/Imports/VehiclesImport.php
+++ app/Imports/VehiclesImport.php
@@ -4,15 +4,16 @@ namespace App\Imports;
 
 use App\Models\Vehicle; // Make sure this points to your Vehicle model
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class VehiclesImport implements ToModel
+class VehiclesImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
         return new Vehicle([
-            'vehicle_name' => $row[0],
-            'category' => $row[1],
-            'plate_number' => $row[2],
+            'vehicle_name' => $row['vehicle_name'], // Use the header names
+            'category'     => $row['category'],     // Use the header names
+            'plate_number' => $row['plate_number'], // Use the header names
         ]);
     }
 }
