bf1ae11f05b6c8c0ca6d0f5795fcb8d3700c32e0
mvc <nyemamudhihir@gmail.com>
2024-11-22 01:28:57 +0300

    commit

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -22,11 +22,11 @@ class AssignmentController extends Controller
         $this->middleware('auth:sanctum')->except(['register', 'login']);
     }
 
-  public function index()
+ public function index()
 {
     try {
         // Retrieve assignments for the logged-in user where status is null
-        $assignments = Assignment::with('customer','user') // Eager load the customer relationship
+        $assignments = Assignment::with('customer', 'user') // Eager load the customer and user relationship
             ->where('user_id', Auth::id()) // Filter by the logged-in user's user_id
             ->whereNull('status') // Only include assignments where status is null
             ->orderBy('assignment_id', 'desc') // Order by assignment_id descending
@@ -44,12 +44,12 @@ class AssignmentController extends Controller
                 'plate_number' => $assignment->plate_number,
                 'customer_phone' => $assignment->customer_phone,
                 'location' => $assignment->location,
-                'case_reported'  => $assignment->case_reported,
-                'customer_debt'  => $assignment->customer_debt,
+                'case_reported' => $assignment->case_reported,
+                'customer_debt' => $assignment->customer_debt,
                 'assigned_by' => $assignment->assigned_by,
                 'customername' => $assignment->customer->customername ?? 'N/A', // Get customer name, or 'N/A' if not available
-                 'created_at' => $assignment->created_at->format('m-d-Y'),
-                 'user_email' => $assignment->user->email ?? 'Unknown email',
+                'created_at' => $assignment->created_at->format('m-d-Y'),
+                'user_email' => $assignment->user->email ?? 'Unknown email', // Access user's email
                 'days_passed' => $daysPassed, // Add the days passed field
             ];
         });
@@ -70,6 +70,7 @@ class AssignmentController extends Controller
         ], 500);
     }
 }
+
 
 public function fetchcustomer()
 {
